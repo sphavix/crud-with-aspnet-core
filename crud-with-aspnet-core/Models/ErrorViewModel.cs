5bd54ae40ffa2e01faf205896e548adb1e2aa4b0
Administrator <Administrator@AZSVR001>
2022-05-20 08:47:28 +0200

    Add project files.

---
+++ crud-with-aspnet-core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace crud_with_aspnet_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
